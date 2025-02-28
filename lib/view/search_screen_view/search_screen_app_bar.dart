45182847fd31404ac49370ff0d4c2a4157a7176f
mzee-flutter <mudair.khan37@gmail.com>
2025-02-28 20:36:13 +0400

    this is search-screen appbar

---
+++ lib/view/search_screen_view/search_screen_app_bar.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SearchScreenAppBar extends StatelessWidget
+    implements PreferredSizeWidget {
+  const SearchScreenAppBar({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      title: Text(
+        'Explore..',
+        style: Theme.of(context).textTheme.displayLarge,
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
