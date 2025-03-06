34f2616a19e1b88dff7cbf9f024423a00a8bd28e
mzee-flutter <mudair.khan37@gmail.com>
2025-03-06 10:39:06 +0400

    these are screen name to which we navigate

---
+++ lib/utils/routes/routes_names.dart
@@ -0,0 +1,8 @@
+class RoutesName {
+  static const String homeScreen = 'home_screen';
+  static const String searchScreen = 'search_screen';
+  static const String bookMarkScreen = 'book_mark_screen';
+  static const String libraryScreen = 'library_screen';
+  static const String profileScreen = 'profile_screen';
+  static const String moreBooksScreen = 'more_books_screen';
+}
