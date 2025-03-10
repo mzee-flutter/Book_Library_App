3703d27c4ffabc862a3577ac9b479a3c39206595
mzee-flutter <mudair.khan37@gmail.com>
2025-03-10 22:57:24 +0400

    updates in the routes

--- lib/utils/routes/routes.dart
+++ lib/utils/routes/routes.dart
@@ -1,13 +1,10 @@
 import 'package:BookMate_Pro/utils/routes/routes_names.dart';
 import 'package:BookMate_Pro/view/book_mark_screen_view/book_mark_screen.dart';
 import 'package:BookMate_Pro/view/home_screen_view/home_screen.dart';
-import 'package:BookMate_Pro/view/home_screen_view/more_books_screen.dart';
 import 'package:BookMate_Pro/view/library_screen_view/library_screen.dart';
 import 'package:BookMate_Pro/view/profile_screen_view/profile_screen.dart';
 import 'package:BookMate_Pro/view/search_screen_view/search_screen.dart';
 import 'package:flutter/material.dart';
-
-import '../../view/single_book_screen/single_book_screen.dart';
 
 class Routes {
   static MaterialPageRoute generateRouts(RouteSettings settings) {
@@ -26,9 +23,6 @@ class Routes {
 
       case RoutesName.profileScreen:
         return MaterialPageRoute(builder: (_) => const ProfileScreen());
-
-      case RoutesName.moreBooksScreen:
-        return MaterialPageRoute(builder: (_) => const MoreBooksScreen());
 
       default:
         return MaterialPageRoute(
