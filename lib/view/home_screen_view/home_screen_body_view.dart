3303116920a3dd0926e295b76dae69d46e48af5a
mzee-flutter <mudair.khan37@gmail.com>
2025-03-11 11:36:51 +0400

    adding more category of books to the home-screen

--- lib/view/home_screen_view/home_screen_body_view.dart
+++ lib/view/home_screen_view/home_screen_body_view.dart
@@ -1,10 +1,13 @@
 import 'package:BookMate_Pro/utils/custom_book_card/medium_book_card.dart';
+import 'package:BookMate_Pro/view/home_screen_view/more_books_screen.dart';
 import 'package:BookMate_Pro/view/single_book_screen/single_book_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 import '../../utils/books_category_and_more.dart';
 import '../../utils/routes/routes_names.dart';
+import '../../view_model/converstion_books_view_model.dart';
 import '../../view_model/google_books_view_model.dart';
 
 class HomeScreenBodyView extends StatefulWidget {
@@ -20,8 +23,11 @@ class HomeScreenBodyViewState extends State<HomeScreenBodyView> {
     super.initState();
     final booksProvider =
         Provider.of<GoogleBooksViewModel>(context, listen: false);
+    final conversationBookProvider =
+        Provider.of<ConversationBooksViewModel>(context, listen: false);
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
       booksProvider.fetchGoogleBooksList();
+      conversationBookProvider.fetchGoogleConversationBooksList();
     });
   }
 
@@ -32,79 +38,161 @@ class HomeScreenBodyViewState extends State<HomeScreenBodyView> {
 
   @override
   Widget build(BuildContext context) {
+    final bPro = Provider.of<GoogleBooksViewModel>(context, listen: false);
+    final converPro =
+        Provider.of<ConversationBooksViewModel>(context, listen: false);
     return SafeArea(
       child: Padding(
         padding: EdgeInsets.symmetric(
           horizontal: 15.r,
           vertical: 7.r,
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            BooksCategoryAndMore(
-              categoryName: 'Top Self-Help Books',
-              categoryDescription: 'Your journey to a better you begins here.',
-              categoryIcon: Icons.lightbulb_outline,
-              onTap: () {
-                Navigator.pushNamed(context, RoutesName.moreBooksScreen);
-              },
-            ),
-            Container(
-              height: 310.h,
-              child: Consumer<GoogleBooksViewModel>(
-                builder: (context, booksProvider, child) {
-                  return NotificationListener<ScrollNotification>(
-                    onNotification: (scrollInfo) {
-                      if (_isNearToEnd(scrollInfo)) {
-                        if (!booksProvider.isFetching &&
-                            booksProvider.allBooks.length < 30) {
-                          booksProvider
-                              .fetchGoogleBooksList(); // Trigger load more books
-                        }
-                      }
-                      return true;
-                    },
-                    child: GridView.builder(
-                      scrollDirection: Axis.horizontal,
-                      shrinkWrap: false,
-                      itemCount: booksProvider.allBooks.length +
-                          (booksProvider.isFetching
-                              ? 1
-                              : 0), // Add 1 for loading indicator
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 1,
-                          mainAxisExtent: 175.h, // Adjusted to fit image + text
-                          crossAxisSpacing: 10.w,
-                          mainAxisSpacing: 8.w),
-                      itemBuilder: (context, index) {
-                        if (index == booksProvider.allBooks.length &&
-                            booksProvider.isFetching) {
-                          return const Center(
-                              child: CircularProgressIndicator());
-                        }
-
-                        final book = booksProvider.allBooks[index];
-
-                        return MediumBookCard(
-                          book: book,
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (_) =>
-                                    SingleBookScreen(fullBook: book),
-                              ),
-                            );
-                          },
-                        );
-                      },
-                    ),
-                  );
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              BooksCategoryAndMore(
+                categoryName: 'Top Self-Help Books',
+                categoryDescription:
+                    'Your journey to a better you begins here.',
+                categoryIcon: Icons.lightbulb_outline,
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (_) =>
+                              MoreBooksScreen(listOfBooks: bPro.allBooks)));
                 },
               ),
-            ),
-          ],
+              Container(
+                height: 310.h,
+                child: Consumer<GoogleBooksViewModel>(
+                  builder: (context, booksProvider, child) {
+                    return NotificationListener<ScrollNotification>(
+                      onNotification: (scrollInfo) {
+                        if (_isNearToEnd(scrollInfo)) {
+                          if (!booksProvider.isFetching &&
+                              booksProvider.allBooks.length < 30) {
+                            booksProvider
+                                .fetchGoogleBooksList(); // Trigger load more books
+                          }
+                        }
+                        return true;
+                      },
+                      child: GridView.builder(
+                        scrollDirection: Axis.horizontal,
+                        shrinkWrap: false,
+                        itemCount: booksProvider.allBooks.length +
+                            (booksProvider.isFetching
+                                ? 1
+                                : 0), // Add 1 for loading indicator
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 1,
+                            mainAxisExtent:
+                                175.h, // Adjusted to fit image + text
+                            crossAxisSpacing: 10.w,
+                            mainAxisSpacing: 8.w),
+                        itemBuilder: (context, index) {
+                          if (index == booksProvider.allBooks.length &&
+                              booksProvider.isFetching) {
+                            return const Center(
+                                child: CircularProgressIndicator());
+                          }
+
+                          final book = booksProvider.allBooks[index];
+
+                          return MediumBookCard(
+                            book: book,
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (_) =>
+                                      SingleBookScreen(fullBook: book),
+                                ),
+                              );
+                            },
+                          );
+                        },
+                      ),
+                    );
+                  },
+                ),
+              ),
+              BooksCategoryAndMore(
+                  categoryName: 'Conversation & Communication',
+                  categoryDescription:
+                      'Master the art of meaningful conversations and effective communication.',
+                  categoryIcon: FontAwesomeIcons.comment,
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) =>
+                            MoreBooksScreen(listOfBooks: converPro.allBooks),
+                      ),
+                    );
+                  }),
+              Container(
+                height: 310.h,
+                child: Consumer<ConversationBooksViewModel>(
+                  builder: (context, conversationBooksProvider, child) {
+                    return NotificationListener<ScrollNotification>(
+                      onNotification: (scrollInfo) {
+                        if (_isNearToEnd(scrollInfo)) {
+                          if (!conversationBooksProvider.isFetching &&
+                              conversationBooksProvider.allBooks.length < 30) {
+                            conversationBooksProvider
+                                .fetchGoogleConversationBooksList(); // Trigger load more books
+                          }
+                        }
+                        return true;
+                      },
+                      child: GridView.builder(
+                        scrollDirection: Axis.horizontal,
+                        shrinkWrap: false,
+                        itemCount: conversationBooksProvider.allBooks.length +
+                            (conversationBooksProvider.isFetching
+                                ? 1
+                                : 0), // Add 1 for loading indicator
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 1,
+                            mainAxisExtent:
+                                175.h, // Adjusted to fit image + text
+                            crossAxisSpacing: 10.w,
+                            mainAxisSpacing: 8.w),
+                        itemBuilder: (context, index) {
+                          if (index ==
+                                  conversationBooksProvider.allBooks.length &&
+                              conversationBooksProvider.isFetching) {
+                            return const Center(
+                                child: CircularProgressIndicator());
+                          }
+
+                          final book =
+                              conversationBooksProvider.allBooks[index];
+
+                          return MediumBookCard(
+                            book: book,
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (_) =>
+                                      SingleBookScreen(fullBook: book),
+                                ),
+                              );
+                            },
+                          );
+                        },
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
