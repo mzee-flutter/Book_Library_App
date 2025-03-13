ad2915d1ea16d7b3cc32ea7fabd61124663d81f3
mzee-flutter <mudair.khan37@gmail.com>
2025-03-13 11:31:11 +0400

    customizing the cards class to reduce the length of the code

--- lib/view/search_screen_view/search_screen.dart
+++ lib/view/search_screen_view/search_screen.dart
@@ -1,6 +1,6 @@
+import 'package:BookMate_Pro/utils/custom_book_card/clickable_category_cards.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -10,30 +10,7 @@ class SearchScreen extends StatefulWidget {
 }
 
 class SearchScreenState extends State<SearchScreen> {
-  final Map<String, IconData> bookCategories = {
-    "Money": FontAwesomeIcons.dollarSign,
-    "Business": FontAwesomeIcons.briefcase,
-    "Mindfulness": FontAwesomeIcons.brain,
-    "Leadership": FontAwesomeIcons.userTie,
-    "Wealth": FontAwesomeIcons.gem,
-    "Success": FontAwesomeIcons.trophy,
-    "Meditation": FontAwesomeIcons.om,
-    "Habit": FontAwesomeIcons.listCheck,
-    "Romance": FontAwesomeIcons.heart,
-    "Philosophy": FontAwesomeIcons.book,
-    "History": FontAwesomeIcons.landmark,
-    "Science": FontAwesomeIcons.flask,
-    "Health": FontAwesomeIcons.heartbeat,
-    "Lifestyle": FontAwesomeIcons.bicycle,
-    "Entrepreneurship": FontAwesomeIcons.chartLine,
-    "Culture": FontAwesomeIcons.theaterMasks,
-    "Psychology": FontAwesomeIcons.brain,
-    "Nature": FontAwesomeIcons.tree,
-    "Marketing & Sales": FontAwesomeIcons.bullhorn,
-    "Religion & Spirituality": FontAwesomeIcons.prayingHands,
-    "Islam": FontAwesomeIcons.mosque,
-  };
-
+  final ClickableCategoryCards _cards = ClickableCategoryCards();
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -84,64 +61,18 @@ class SearchScreenState extends State<SearchScreen> {
             SizedBox(
               height: 10.r,
             ),
-            Container(
-              color: Colors.blue,
-              height: 420,
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    Wrap(
-                      spacing: 10,
-                      runSpacing: 10,
-                      children: bookCategories.entries
-                          .map((entry) => CategoryCard(
-                              category: entry.key, icon: entry.value))
-                          .toList(),
-                    ),
-                  ],
+            Expanded(
+              child: Container(
+                child: SingleChildScrollView(
+                  child: Wrap(
+                    alignment: WrapAlignment.center,
+                    spacing: 10,
+                    runSpacing: 7,
+                    children: _cards.getCardList(),
+                  ),
                 ),
               ),
             )
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class CategoryCard extends StatelessWidget {
-  final String category;
-  final IconData icon;
-
-  const CategoryCard({super.key, required this.category, required this.icon});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
-      elevation: 2,
-      child: Container(
-        padding: const EdgeInsets.all(10),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Icon(
-              icon,
-              size: Theme.of(context)
-                  .bottomNavigationBarTheme
-                  .selectedIconTheme
-                  ?.size,
-              color:
-                  Theme.of(context).bottomNavigationBarTheme.selectedItemColor,
-            ),
-            SizedBox(
-              width: 15.r,
-            ),
-            Text(
-              category,
-              textAlign: TextAlign.center,
-            ),
           ],
         ),
       ),
