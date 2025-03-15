98109fb03e6a97244338dce3bdb89a988d69f1f4
mzee-flutter <mudair.khan37@gmail.com>
2025-03-15 15:21:05 +0400

    updates...

--- lib/view/home_screen_view/more_books_screen.dart
+++ lib/view/home_screen_view/more_books_screen.dart
@@ -1,12 +1,18 @@
 import 'package:BookMate_Pro/view/single_book_screen/single_book_screen.dart';
 import 'package:BookMate_Pro/view_model/more_books_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 import '../../utils/custom_book_card/small_book_card.dart';
 
 class MoreBooksScreen extends StatefulWidget {
-  const MoreBooksScreen({super.key, required this.category});
+  const MoreBooksScreen({
+    super.key,
+    required this.category,
+    required this.name,
+  });
   final String category;
+  final String name;
 
   @override
   MoreBooksScreenState createState() => MoreBooksScreenState();
@@ -32,49 +38,51 @@ class MoreBooksScreenState extends State<MoreBooksScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Self-Help Books'),
-        centerTitle: true,
+        title: Text(widget.name),
         elevation: 5,
         iconTheme: const IconThemeData(
           size: 25,
         ),
       ),
       body: SafeArea(
-        child: Consumer<MoreBooksViewModel>(
-          builder: (context, booksProvider, child) {
-            return NotificationListener<ScrollNotification>(
-              onNotification: (scrollInfo) {
-                if (_isScrollNearToEnd(scrollInfo)) {
-                  if (!booksProvider.isFetching) {
-                    booksProvider.fetchMoreBooksList(widget.category);
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 5.r, vertical: 10.r),
+          child: Consumer<MoreBooksViewModel>(
+            builder: (context, booksProvider, child) {
+              return NotificationListener<ScrollNotification>(
+                onNotification: (scrollInfo) {
+                  if (_isScrollNearToEnd(scrollInfo)) {
+                    if (!booksProvider.isFetching) {
+                      booksProvider.fetchMoreBooksList(widget.category);
+                    }
                   }
-                }
-                return true;
-              },
-              child: ListView.builder(
-                itemCount: booksProvider.allBooks.length +
-                    (booksProvider.isFetching ? 1 : 0),
-                itemBuilder: (context, index) {
-                  if (index == booksProvider.allBooks.length &&
-                      booksProvider.isFetching) {
-                    return const Center(child: CircularProgressIndicator());
-                  }
-
-                  final book = booksProvider.allBooks[index];
-                  return SmallBookCard(
-                    book: book,
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (_) =>
-                                  SingleBookScreen(fullBook: book)));
-                    },
-                  );
+                  return true;
                 },
-              ),
-            );
-          },
+                child: ListView.builder(
+                  itemCount: booksProvider.allBooks.length +
+                      (booksProvider.isFetching ? 1 : 0),
+                  itemBuilder: (context, index) {
+                    if (index == booksProvider.allBooks.length &&
+                        booksProvider.isFetching) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+
+                    final book = booksProvider.allBooks[index];
+                    return SmallBookCard(
+                      book: book,
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (_) =>
+                                    SingleBookScreen(fullBook: book)));
+                      },
+                    );
+                  },
+                ),
+              );
+            },
+          ),
         ),
       ),
     );
