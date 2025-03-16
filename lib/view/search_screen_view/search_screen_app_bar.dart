a8f0498bbfb8510c01eb41cf4b4e3fb4f4f9f4e4
mzee-flutter <mudair.khan37@gmail.com>
2025-03-16 10:48:33 +0400

    making appbar as textfield for simplicity

--- lib/view/search_screen_view/search_screen_app_bar.dart
+++ lib/view/search_screen_view/search_screen_app_bar.dart
@@ -1,16 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+
+import '../../utils/snakebars_and_popUps/snake_bars.dart';
+import '../../view_model/search_view_model.dart';
 
 class SearchScreenAppBar extends StatelessWidget
     implements PreferredSizeWidget {
   const SearchScreenAppBar({super.key});
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      automaticallyImplyLeading: false,
-      title: Text(
-        'Explore...',
-        style: Theme.of(context).textTheme.displayLarge,
-      ),
+    return Consumer<SearchViewModel>(
+      builder: (context, searchProvider, child) {
+        return AppBar(
+          automaticallyImplyLeading: false,
+          elevation: 3,
+          leading: const Icon(Icons.arrow_back_rounded),
+          actions: [
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.only(left: 40.r),
+                child: TextFormField(
+                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 17,
+                      ),
+                  controller: searchProvider.controller,
+                  decoration: InputDecoration(
+                    isDense: true,
+                    hintText: 'Search Book Here...',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide.none),
+                    suffixIcon: InkWell(
+                      onTap: () {
+                        searchProvider.clearSearch();
+                      },
+                      child: const Icon(Icons.close),
+                    ),
+                  ),
+                  onFieldSubmitted: (value) {
+                    final title = searchProvider.controller.text.trim();
+                    title.isEmpty
+                        ? SnakeBars.flutterToast('Invalid Input')
+                        : searchProvider.fetchBooksBySearch(title);
+                  },
+                ),
+              ),
+            )
+          ],
+        );
+      },
     );
   }
 
