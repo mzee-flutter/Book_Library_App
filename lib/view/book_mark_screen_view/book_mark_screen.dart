0cb5dbd8d8b1db454e1072eb37ad8da443f95450
mzee-flutter <mudair.khan37@gmail.com>
2025-03-16 23:45:14 +0400

    fetching books from firebase and displaying in book-mark screen

--- lib/view/book_mark_screen_view/book_mark_screen.dart
+++ lib/view/book_mark_screen_view/book_mark_screen.dart
@@ -1,4 +1,9 @@
+import 'package:BookMate_Pro/view/single_book_screen/single_book_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:BookMate_Pro/utils/custom_book_card/small_book_card.dart';
+import 'package:provider/provider.dart';
+
+import '../../view_model/book_mark_view_model.dart';
 
 class BookMarkScreen extends StatefulWidget {
   const BookMarkScreen({super.key});
@@ -9,12 +14,37 @@ class BookMarkScreen extends StatefulWidget {
 
 class BookMarkScreenState extends State<BookMarkScreen> {
   @override
+  void initState() {
+    super.initState();
+    Provider.of<BookMarkViewModel>(context, listen: false).getBookMarks();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: Center(
-        child: Text(
-          'This is the bookmark-page',
-        ),
+    return SafeArea(
+      child: Consumer<BookMarkViewModel>(
+        builder: (context, bookmarkProvider, child) {
+          return Column(
+            children: [
+              ListView.builder(
+                  itemCount: bookmarkProvider.markedBooksList.length,
+                  itemBuilder: (context, index) {
+                    final book = bookmarkProvider.markedBooksList[index];
+
+                    return SmallBookCard(
+                        book: book,
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (_) => SingleBookScreen(fullBook: book),
+                            ),
+                          );
+                        });
+                  })
+            ],
+          );
+        },
       ),
     );
   }
